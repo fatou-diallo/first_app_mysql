56862c84ee004ea9900527c8de952588acb0c6b4
Fatou Diallo <fatoujap1@gmail.com>
2013-02-03 20:38:12 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstAppMysql::Application.config.secret_token = 'df93f61e30984823022281ca8023c6eba5310e072f6a545b6c9d056eb78e2b5cfd4d8aef93d91108e5dad863191f31f49dfb5532aa10c6935b8102f48a796bea'
